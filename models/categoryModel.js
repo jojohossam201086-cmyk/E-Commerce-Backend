893777e99fd96cd8fe33438075f803252dc245a4
jojohossam201086-cmyk <jojohossam201086@gmail.com>
2026-07-08 15:36:45 +0300

    edit

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,12 +1,23 @@
 const mongoose = require("mongoose");
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    slug: {
+      type: String,
+    },
   },
-  description: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Category", categorySchema);
